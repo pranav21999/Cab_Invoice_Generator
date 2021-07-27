8c6d06683d8708dc85a27eaf2e939905eef2858c
pranav21999 <pranavdani99@gmail.com>
2021-07-28 00:53:10 +0530

    Added:class to calculate totalfare,TotalNoOfRides,AverageRides for multiple rides

--- src/main/java/com/cab/CabServiceTest.java
+++ src/main/java/com/cab/CabServiceTest.java
@@ -1,6 +1,5 @@
 package com.cab;
 
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,38 +9,46 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CabServiceTest {
-    private static CabService cabService;
+	private static CabService cabService;
 
+	@Before
+	public void setup() {
+		cabService = new CabService();
+	}
 
-    @Before
-    public void setup(){
-        cabService = new CabService();
-    }
+	@Test
+	public void givenDistanceAndTimeShouldReturnTotalFare() {
+		double distance = 10;
+		int time = 15;
+		double totalFare = cabService.invoiceGenerator(distance, time);
+		Assert.assertEquals(115.0, totalFare, 0.0);
 
-    @Test
-    public void givenDistanceAndTimeShouldReturnTotalFare(){
-        double distance = 10;
-        int time = 15;
-        double totalFare = cabService.invoiceGenerator(distance,time);
-        Assert.assertEquals(115.0,totalFare,0.0);
+	}
 
-    }
-    @Test
-    public void givenDistanceAndTimeShouldReturnDefaultFare(){
-        double distance = 0.2;
-        int time = 3;
-        double totalFare = cabService.invoiceGenerator(distance,time);
-        Assert.assertEquals(5,totalFare,0.0);
+	@Test
+	public void givenDistanceAndTimeShouldReturnDefaultFare() {
+		double distance = 0.2;
+		int time = 3;
+		double totalFare = cabService.invoiceGenerator(distance, time);
+		Assert.assertEquals(5, totalFare, 0.0);
 
-    }
-    @Test
-    public void givenMultipleRidesShouldReturnAggregateTotal(){
-        Ride ride1 = new Ride(10.0,15);
-        Ride ride2 = new Ride(20.0,30);
-        List<Ride> rides = Arrays.asList(ride1,ride2);
-        double aggFare = cabService.invoiceGenerator(rides);
-        Assert.assertEquals(345,aggFare,0.0);
+	}
 
-    }
-
+//    @Test
+//    public void givenMultipleRidesShouldReturnAggregateTotal(){
+//        Ride ride1 = new Ride(10.0,15);
+//        Ride ride2 = new Ride(20.0,30);
+//        List<Ride> rides = Arrays.asList(ride1,ride2);
+//        double aggFare = cabService.invoiceGenerator(rides);
+//        Assert.assertEquals(345,aggFare,0.0);
+//    }
+	@Test
+	public void givenMultipleRidesShouldReturnInvoiceSummary() {
+		Ride ride1 = new Ride(10.0, 5);
+		Ride ride2 = new Ride(20.0, 5);
+		List<Ride> rides = Arrays.asList(ride1, ride2);
+		InvoiceSummary summary = cabService.invoiceGenerator(rides);
+		InvoiceSummary expected = new InvoiceSummary(2, 310.0);
+		Assert.assertEquals(expected, summary);
+	}
 }
